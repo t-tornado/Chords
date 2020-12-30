1c84b431378287d175a4f2a0eea675e0c5a8ca87
t-tornado <anthonyamp0011@gmail.com>
2020-12-30 12:35:25 +0000

    Config file

---
+++ Config/Dimensions.js
@@ -0,0 +1,27 @@
+import {Dimensions} from 'react-native'
+
+const {height, width} = Dimensions.get('window')
+
+export const fifteen = width * 0.03826
+export const fourteen = width * (14/width)
+export const eighteen = width * (18/width)
+export const thirteen = width * (13 / width)
+export const eleven = width * (11/width)
+export const thiry_two = width * 0.0815
+export const thirty = width * (30 / width)
+export const ten = width * (10/width)
+export const twenty_five = width * (25/width)
+export const twenty_eight = width * (28/width)
+export const forty = width * 0.1019
+export const fifty = width * 0.1274
+export const forty_five = width * (45/width)
+export const twenty = width * 0.05093
+export const five =width * ( 5 / width)
+export const three = width * (3/width)
+export const seven = width * 0.01783
+export const thirty_three = width * (33 / width)
+export const seventeen = width * (17 / width )
+export const twelve = width * (12/ width)
+export const hundred_eighty = width * (90/width)
+export const ninety_five = width * (95/width)
+export const eighty = width * (80/width)
